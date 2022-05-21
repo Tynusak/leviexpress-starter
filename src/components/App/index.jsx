4a15219afd5d6f03dee44cc3e6471c4a37b85308
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-05-21 10:42:05 +0200

    Hotové routování

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <>
-    <Header />
-    <Home />
-    <Footer />
+    {' '}
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
   </>
 );
