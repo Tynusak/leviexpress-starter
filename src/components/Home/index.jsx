d120e55b1730b08db58a972f70680a40e72ef57f
Kristýna Švandová <kristyna.svandova@gmail.com>
2022-05-24 12:09:14 +0200

    Hotová 1. část

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { JourneyPicker } from '../JourneyPicker';
 import { JourneyDetail } from '../JourneyDetail';
 import { SelectedSeat } from '../SelectedSeat';
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
+  let navigate = useNavigate();
+  const handleBuy = () =>
+    fetch('https://apps.kodim.cz/daweb/leviexpress/api/reservation', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        action: 'create',
+        seat: journey.autoSeat,
+        journeyId: journey.journeyId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => navigate(`/reservation/${data.results.reservationId}`));
 
   return (
     <main>
@@ -17,7 +33,12 @@ export const Home = () => {
       {journey === null ? null : (
         <>
           <JourneyDetail journey={journey} />
-          <SelectedSeat number={journey.autoSeat} />
+          <SelectedSeat number={journey.autoSeat} />{' '}
+          <div className="controls container">
+            <button className="btn btn--big" type="button" onClick={handleBuy}>
+              Rezervovat
+            </button>
+          </div>
         </>
       )}
     </main>
